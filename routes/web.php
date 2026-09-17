1124fe80d927e3506132ad7130a438d4e5a4f690
rzkasufianty <rzka.sufianty@gmail.com>
2026-09-17 16:56:57 +0700

    Pertemuan 2 Latihan PHP Dasar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/latihan-php', function () {
+    $nama = 'Rizka Sufianty';
+    $nilai = [80, 75, 90, 85, 88];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+
+        return $total / count($data);
+    };
+
+$rataRata = $hitungRataRata($nilai);
+
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
+});
